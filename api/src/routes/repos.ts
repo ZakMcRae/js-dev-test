85f17f056f3455f0496390654b884a4071ce1a53
Zak McRae <zakmcrae@gmail.com>
2021-12-02 13:24:03 -0500

    brought in repo data and combined into 1 source
    imported from json file and from Github API

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,8 @@
 import { Router, Request, Response } from 'express';
+import axios from 'axios';
+
+// bring in json repo data
+import jsonRepoData from '../../data/repos.json';
 
 export const repos = Router();
 
@@ -7,6 +11,15 @@ repos.get('/', async (_: Request, res: Response) => {
 
   res.status(200);
 
+  // bring in API repo data
+  const response = await axios.get(
+    'https://api.github.com/users/silverorange/repos'
+  );
+
+  // combine both repo data sources
+  const allRepos = [...jsonRepoData, ...response.data];
+
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  // res.json([]);
+  res.json(allRepos);
 });
