fb3291332e443553963bf64bb1e6f0731b68b083
Zak McRae <zakmcrae@gmail.com>
2021-12-06 09:34:22 -0500

    added readme to RepoDetail popup if it exists
    attempts to fetch readme from github api
    if it exists render it with ReactMarkdown component

--- web/src/components/RepoDetail.js
+++ web/src/components/RepoDetail.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 // a pop up which displays details of a specific repository
 function RepoDetail({ repoToDisplay }) {
   const [commitDetail, setCommitDetail] = useState(null);
+  const [readmeDetail, setReadmeDetail] = useState(null);
 
-  // get commit details from github api when repoToDisplay changes in state
+  // get commit details and readme from github api when repoToDisplay changes in state
   useEffect(() => {
-    // catch Json Examples - not real repos with actual commit details
+    // catch Json Examples - not real repos with actual commit details or a readme
     if (repoToDisplay.includes('Example')) {
       setCommitDetail({
         author: {
           name: 'silverorange',
         },
       });
+      setReadmeDetail(null);
     } else {
-      const fetchData = async () => {
+      const fetchCommitData = async () => {
         const response = await fetch(
           `https://api.github.com/repos/${repoToDisplay}/commits?per_page=1`,
           {
@@ -26,7 +29,26 @@ function RepoDetail({ repoToDisplay }) {
         setCommitDetail(json[0].commit);
       };
 
-      fetchData();
+      const fetchReadmeData = async () => {
+        const response = await fetch(
+          `https://raw.githubusercontent.com/${repoToDisplay}/master/README.md`,
+          {
+            mode: 'cors',
+          }
+        );
+
+        // check if api returns readme or not
+        if (response.status !== 200) {
+          setReadmeDetail(null);
+        } else {
+          const readme = await response.text();
+          // store fetched data in commitDetail in state
+          setReadmeDetail(readme);
+        }
+      };
+
+      fetchCommitData();
+      fetchReadmeData();
     }
   }, [repoToDisplay]);
 
@@ -35,16 +57,17 @@ function RepoDetail({ repoToDisplay }) {
       {commitDetail ? (
         <>
           <p>Repo Name: {repoToDisplay.split('/')[1]}</p>
-          <p>Author: {commitDetail.author.name || 'N/A'}</p>
+          <p>Commit Author: {commitDetail.author.name || 'N/A'}</p>
           <p>
-            Date:{' '}
+            Commit Date:{' '}
             {commitDetail.author.date
               ? new Date(commitDetail.author.date).toLocaleString('en-ca')
               : 'N/A'}
           </p>
-          <p>Message: {commitDetail.message || 'N/A'}</p>
+          <p>Commit Message: {commitDetail.message || 'N/A'}</p>
         </>
       ) : null}
+      {readmeDetail ? <ReactMarkdown>{readmeDetail}</ReactMarkdown> : null}
     </div>
   );
 }
